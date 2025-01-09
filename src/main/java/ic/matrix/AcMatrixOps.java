18dfbeb92b1bb8ccf805c1ac92a0f163ae8a2ee9
Smyslov.Aleksey <a.smisloff@gmail.com>
2025-01-09 11:23:11 +0300

    KZKT: вар. 3

--- src/main/java/ic/matrix/AcMatrixOps.java
+++ src/main/java/ic/matrix/AcMatrixOps.java
@@ -3,148 +3,21 @@ package ic.matrix;
 import org.ejml.data.ZMatrixRMaj;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import ru.vniizht.asuterkortes.counter.latticemodel.DynamicComplexArray;
-import ru.vniizht.asuterkortes.counter.latticemodel.DynamicIntArray;
 
 import java.util.Arrays;
 
-import static java.lang.System.nanoTime;
-
 public class AcMatrixOps {
 
-    public static long t1 = 0;
-    public static long t2 = 0;
-
-    public static ZMatrixRMaj KZKT(
-            @NotNull IMatrixCsr K,
-            @NotNull ZMatrixCsr Z,
-            @NotNull IMatrixCsr KT,
-            @NotNull DynamicComplexArray tmpv,
-            @NotNull DynamicIntArray tmpc,
-            @NotNull DynamicIntArray tmpi,
-            @Nullable ZMatrixRMaj dest
-    ) {
+    public static ZMatrixRMaj KZ(@NotNull IMatrixCsr K, @NotNull ZMatrixCsr Z, @Nullable ZMatrixRMaj dest) {
         if (dest == null) {
-            dest = new ZMatrixRMaj(K.numRows(), K.numRows());
+            dest = new ZMatrixRMaj(K.numRows(), K.numCols());
         } else {
-            dest.reshape(K.numRows(), K.numRows());
-            dest.zero();
-        }
-        tmpv.setSize(Z.numCols());
-        tmpc.setSize(Z.numCols());
-        tmpi.setSize(Z.numCols());
-        Arrays.fill(tmpi.getData(), 0, tmpi.getSize(), -1);
-        int nzCnt;
-        for (int i = 0, a = 0; i < K.numRows(); i++, a += 2 * K.numRows()) {
-            nzCnt = 0;
-            long start = nanoTime();
-            for (int j = K.begin(i); j < K.end(i); j++) {
-                int colIdx = K.cols.get(j);
-                int k = K.data.get(j);
-                for (int l = Z.begin(colIdx); l < Z.end(colIdx); l++) {
-                    double re = Z.data.getRe(l) * k;
-                    double im = Z.data.getIm(l) * k;
-                    int idx = Z.cols.get(l);
-                    if (tmpi.get(idx) < i) {
-                        tmpi.set(idx, i);
-                        tmpc.set(nzCnt++, idx);
-                        tmpv.set(idx, re, im);
-                    } else {
-                        tmpv.getDataRe()[idx] += re;
-                        tmpv.getDataIm()[idx] += im;
-                    }
-                }
+            int numberOfElements = K.numRows() * K.numCols();
+            if (dest.numRows * dest.numCols >= numberOfElements) {
+                Arrays.fill(dest.data, 0, numberOfElements * 2, 0);
             }
-            long start2 = nanoTime();
-            t1 += start2 - start;
-            for (int j = 0; j < nzCnt; j++) {
-                int idx = tmpc.get(j);
-                double re = tmpv.getRe(idx);
-                double im = tmpv.getIm(idx);
-                for (int m = KT.begin(idx); m < KT.end(idx); m++) {
-                    int c = KT.cols.get(m);
-                    if (c > i) {
-                        break;
-                    }
-                    int k = KT.data.get(m);
-                    int l = a + 2 * c;
-                    dest.data[l] += re * k;
-                    dest.data[l + 1] += im * k;
-                }
-            }
-            t2 += nanoTime() - start2;
+            dest.reshape(K.numRows(), K.numCols());
         }
-        for (int i = 0; i < K.numRows(); i++) {
-            for (int j = 0; j < i; j++) {
-                int ij = 2 * (i * K.numRows() + j);
-                int ji = 2 * (j * K.numRows() + i);
-                dest.data[ji] = dest.data[ij];
-                dest.data[ji + 1] = dest.data[ij + 1];
-            }
-        }
-        return dest;
-    }
-
-    public static ZMatrixCsr transpose(@NotNull ZMatrixCsr src, @Nullable ZMatrixCsr dest) {
-        if (dest == null) {
-            dest = new ZMatrixCsr(src.numRows());
-        } else {
-            dest.reset(src.numRows());
-        }
-        dest.cols.setSize(src.cols.getSize());
-        dest.data.setSize(src.data.getSize());
-        dest.rows.setSize(src.numCols() + 1);
-        int[] destRowsData = dest.rows.getData();
-        Arrays.fill(destRowsData, 0, dest.rows.getSize(), 0);
-        for (int i = 0; i < src.cols.getSize(); i++) {
-            destRowsData[src.cols.get(i) + 1]++;
-        }
-        int cumSum = 0;
-        int tmp;
-        for (int i = 1; i <= src.numCols(); i++) {
-            tmp = destRowsData[i];
-            destRowsData[i] = cumSum;
-            cumSum += tmp;
-        }
-        for (int i = 0; i < src.numRows(); i++) {
-            for (int j = src.begin(i); j < src.end(i); j++) {
-                double re = src.data.getRe(j);
-                double im = src.data.getIm(j);
-                int destRowIdx = src.cols.get(j);
-                int destIdx = destRowsData[destRowIdx + 1];
-                dest.data.set(destIdx, re, im);
-                dest.cols.set(destIdx, i);
-                destRowsData[destRowIdx + 1]++;
-            }
-        }
-        return dest;
-    }
-
-    public static void mul(KMatrix K, ZMatrixAc Z, ZMatrixRMaj dest) {
-        for (int k = 0; k < K.data.length; k++) {
-            double[] kRow = K.data[k];
-            for (int z = 0; z < Z.size(); z++) {
-                double real = 0f;
-                double imag = 0f;
-                for (int idx = Z.begins[z]; idx < Z.ends[z]; idx++) {
-                    int col = Z.cols[idx];
-                    double re = Z.res[idx];
-                    double im = Z.ims[idx];
-                    double kv = kRow[col];
-                    real += re * kv;
-                    imag += im * kv;
-                }
-                dest.set(k, z, real, imag);
-            }
-        }
-    }
-
-    public static void KZ(IMatrixCsr K, ZMatrixCsr Z, ZMatrixRMaj dest) {
-        int numberOfElements = K.numRows() * K.numCols();
-        if (dest.numRows * dest.numCols >= numberOfElements) {
-            Arrays.fill(dest.data, 0, numberOfElements * 2, 0);
-        }
-        dest.reshape(K.numRows(), K.numCols());
         for (int i = 0, a = 0; i < K.numRows(); i++, a += K.numCols() * 2) {
             for (int j = K.begin(i); j < K.end(i); ++j) {
                 int col = K.cols.get(j);
@@ -156,146 +29,34 @@ public class AcMatrixOps {
                 }
             }
         }
-    }
-
-    /**
-     * Операция K&#215Z.
-     *
-     * @param K    Матрица независимых контуров.
-     * @param Z    Мастрица сопротивлений.
-     * @param tmpi Массив для промежуточных данных.
-     * @param tmpz Массив для промежуточных данных.
-     * @param dest Матрица для сохранения результата. Если <code>dest == null</code>, она будет создана.
-     * @return <code>dest</code>.
-     */
-    public static ZMatrixCsr KZ(
-            @NotNull IMatrixCsr K,
-            @NotNull ZMatrixCsr Z,
-            @NotNull DynamicIntArray tmpi,
-            @NotNull DynamicComplexArray tmpz,
-            @Nullable ZMatrixCsr dest
-    ) {
-        if (dest == null) {
-            dest = new ZMatrixCsr(K.numCols());
-        } else {
-            dest.reset(K.numCols());
-        }
-        tmpi.setSize(K.numCols());
-        tmpz.setSize(K.numCols());
-        Arrays.fill(tmpi.getData(), 0, tmpi.getSize(), -1);
-        dest.cols.ensureCapacity(K.numRows() * K.numCols());
-        dest.data.ensureCapacity(K.numRows() * K.numCols());
-        dest.rows.setSize(K.numRows() + 1);
-        int nzCnt = 0;
-        for (int i = 0; i < K.numRows(); i++) {
-            int r = i + 1;
-            for (int j = K.begin(i); j < K.end(i); ++j) {
-                int zRowIdx = K.cols.get(j);
-                int kElt = K.data.get(j);
-                for (int k = Z.begin(zRowIdx); k < Z.end(zRowIdx); k++) {
-                    int zColIdx = Z.cols.get(k);
-                    double re = Z.data.getRe(k) * kElt;
-                    double im = Z.data.getIm(k) * kElt;
-                    if (tmpi.get(zColIdx) < i) {
-                        tmpi.set(zColIdx, i);
-                        dest.cols.set(nzCnt++, zColIdx);
-                        tmpz.set(zColIdx, re, im);
-                    } else {
-                        tmpz.getDataRe()[zColIdx] += re;
-                        tmpz.getDataIm()[zColIdx] += im;
-                    }
-                }
-            }
-            dest.rows.set(r, nzCnt);
-            for (int j = dest.begin(i); j < dest.end(i); j++) {
-                int colIdx = dest.cols.get(j);
-                dest.data.set(j, tmpz.getRe(colIdx), tmpz.getIm(colIdx));
-            }
-        }
-        dest.cols.setSize(nzCnt);
-        dest.data.setSize(nzCnt);
         return dest;
     }
 
-    public static ZMatrixRMaj KZKT(@NotNull ZMatrixCsr KZ, @NotNull IMatrixCsr KT, @Nullable ZMatrixRMaj dest) {
+    public static ZMatrixRMaj KZKT(@NotNull ZMatrixRMaj KZ, @NotNull IMatrixCsr K, @Nullable ZMatrixRMaj dest) {
         if (dest == null) {
-            dest = new ZMatrixRMaj(KZ.numRows(), KZ.numRows());
+            dest = new ZMatrixRMaj(KZ.numRows, KZ.numRows);
         } else {
-            dest.reshape(KZ.numRows(), KZ.numRows());
-            dest.zero();
+            dest.reshape(KZ.numRows, KZ.numRows);
         }
-        for (int i = 0, a = 0; i < KZ.numRows(); i++, a += 2 * KZ.numRows()) {
-            for (int j = KZ.begin(i); j < KZ.end(i); j++) {
-                double re = KZ.data.getRe(j);
-                double im = KZ.data.getIm(j);
-                int ktRowIdx = KZ.cols.get(j);
-                for (int ki = KT.begin(ktRowIdx); ki < KT.end(ktRowIdx); ki++) {
-                    int c = KT.cols.get(ki);
-                    if (c > i) {
-                        break;
-                    }
-                    int k = KT.data.get(ki);
-                    int idx = a + 2 * c;
-                    dest.data[idx] += re * k;
-                    dest.data[idx + 1] += im * k;
-                }
-            }
-        }
-        for (int i = 0; i < KZ.numRows(); i++) {
-            for (int j = 0; j < i; j++) {
-                int ij = 2 * (i * KZ.numRows() + j);
-                int ji = 2 * (j * KZ.numRows() + i);
-                dest.data[ji] = dest.data[ij];
-                dest.data[ji + 1] = dest.data[ij + 1];
-            }
-        }
-        return dest;
-    }
-
-    public static void mul(ZMatrixRMaj M, ZMatrixAc Z, ZMatrixRMaj dest) {
-        int idx = 0;
-        for (int i = 0, anchor = 0; i < M.numRows; i++, anchor += 2 * M.numCols) {
-            for (int j = 0; j < Z.size(); j++) {
-                double re = 0;
-                double im = 0;
-                for (int k = Z.begins[j]; k < Z.ends[j]; k++) {
-                    int col = Z.cols[k];
-                    double zRe = Z.res[k];
-                    double zIm = Z.ims[k];
-                    int mIdx = anchor + 2 * col;
-                    double mRe = M.data[mIdx];
-                    double mIm = M.data[mIdx + 1];
-                    re += (mRe * zRe - mIm * zIm);
-                    im += (mRe * zIm + zRe * mIm);
-                }
-                dest.data[idx++] = re;
-                dest.data[idx++] = im;
-            }
-        }
-    }
-
-    public static void mulTransK(ZMatrixRMaj M, KMatrix K, ZMatrixRMaj dest) {
-        int stride = M.numCols * 2;
-        for (int r = 0, anchor = 0, idx = 0;
-             r < M.numRows;
-             ++r, anchor += stride, idx += r * 2
-        ) {
-            for (int c = r; c < K.numRows(); c++) {
+        int kzStride = KZ.numCols * 2;
+        int kzRowAnchor = 0;
+        int destIdx = 0;
+        for (int i = 0; i < KZ.numRows; ++i) {
+            destIdx += 2 * i;
+            for (int j = i; j < K.numRows(); j++) {
                 double re = 0f;
                 double im = 0f;
-                double[] kRow = K.data[c];
-                int[] nzis = K.nzi[c];
-                int nonZeroEltQty = nzis[0];
-                for (int k = 1; k <= nonZeroEltQty; k++) {
-                    int colIdx = nzis[k];
-                    int mDataIdx = anchor + colIdx * 2;
-                    double kv = kRow[colIdx];
-                    re += M.data[mDataIdx] * kv;
-                    im += M.data[mDataIdx + 1] * kv;
+                for (int k = K.begin(j); k < K.end(j); k++) {
+                    int colIdx = K.cols.get(k);
+                    int kzDataIdx = kzRowAnchor + colIdx * 2;
+                    double kv = K.data.get(k);
+                    re += KZ.data[kzDataIdx] * kv;
+                    im += KZ.data[kzDataIdx + 1] * kv;
                 }
-                dest.data[idx++] = re;
-                dest.data[idx++] = im;
+                dest.data[destIdx++] = re;
+                dest.data[destIdx++] = im;
             }
+            kzRowAnchor += kzStride;
         }
         for (int i = 0; i < K.numRows(); i++) {
             for (int j = i + 1; j < K.numRows(); j++) {
@@ -305,6 +66,7 @@ public class AcMatrixOps {
                 dest.data[ji + 1] = dest.data[ij + 1];
             }
         }
+        return dest;
     }
 
     public static void mul(KMatrix K, VectorAc V, ZMatrixRMaj dest) {
--- src/test/java/ic/matrix/AcMatrixOpsTest.java
+++ src/test/java/ic/matrix/AcMatrixOpsTest.java
@@ -5,167 +5,64 @@ import org.ejml.data.ZMatrixRMaj;
 import org.ejml.dense.row.CommonOps_ZDRM;
 import org.ejml.dense.row.RandomMatrices_ZDRM;
 import org.junit.jupiter.api.Test;
-import ru.vniizht.asuterkortes.counter.latticemodel.DynamicComplexArray;
-import ru.vniizht.asuterkortes.counter.latticemodel.DynamicIntArray;
+import org.junit.jupiter.api.TestInstance;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static ic.matrix.util.IcMatrixTestHelper.*;
+import static ic.matrix.util.IcMatrixTestHelper.measureTimeMillis;
+import static ic.matrix.util.IcMatrixTestHelper.measureTimeMs;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AcMatrixOpsTest {
 
+    private final int timesToRepeat = 20_000;
+    private final int numRows = 30;
+    private final int numCols = 100;
+
+    private final ZMatrixRMaj refZ = randomSymmetricalDenseMatrix(numCols, 0.12);
+    private final ZMatrixCsr Z = zMatrixCsr(refZ);
+    private final ZMatrixRMaj refK = randomDenseKMatrix(numRows, numCols, 10);
+    private final IMatrixCsr K = iMatrixCsr(refK);
+
+    private final ZMatrixRMaj refKZ = new ZMatrixRMaj(refK);
+    private ZMatrixRMaj KZ = new ZMatrixRMaj(refK);
+    private final ZMatrixRMaj refKZKT = new ZMatrixRMaj(refK.numRows, refK.numRows);
+    private ZMatrixRMaj KZKT = new ZMatrixRMaj(refKZKT);
+
+//    @BeforeAll
+//    void prepareTestData() {
+//        Random r = ThreadLocalRandom.current();
+//    }
+
     @Test
-    void transpose() {
-        int numRows = 100;
-        int numCols = 50;
-        ZMatrixRMaj ref = RandomMatrices_ZDRM.rectangle(numRows, numCols, ThreadLocalRandom.current());
-        ZMatrixCsr Z = zMatrixCsr(ref);
-        ZMatrixRMaj refT = CommonOps_ZDRM.transpose(ref, null);
-        ZMatrixCsr ZT = AcMatrixOps.transpose(Z, null);
-        assertArrayEquals(refT.data, rMaj(ZT), 0.5e-6);
+    public void KxZ() {
+        measureTimeMs("ref", timesToRepeat, () -> CommonOps_ZDRM.mult(refK, refZ, refKZ));
+        measureTimeMs("act", timesToRepeat, () -> AcMatrixOps.KZ(K, Z, KZ));
+        assertArrayEquals(refKZ.data, KZ.data, 0.5e-6);
     }
 
     @Test
-    public void arrayFill() {
-        DynamicComplexArray arr = new DynamicComplexArray(100);
-        arr.setSize(1000);
-        long t0 = System.nanoTime();
-        double sum = 0;
-        for (int i = 0; i < 2_000_000; i++) {
-            Arrays.fill(arr.getDataRe(), 0, arr.getSize(), i);
-            Arrays.fill(arr.getDataIm(), 0, arr.getSize(), i);
-            sum += arr.getRe(i % 100) + arr.getIm(i % 100);
-        }
-        System.out.println(sum);
-        System.out.println(msFrom(t0));
+    public void KZxKT() {
+        CommonOps_ZDRM.mult(refK, refZ, refKZ);
+        measureTimeMs("ref", timesToRepeat, () -> CommonOps_ZDRM.multTransB(refKZ, refK, refKZKT));
+        KZ = AcMatrixOps.KZ(K, Z, KZ);
+        measureTimeMs("act", timesToRepeat, () -> AcMatrixOps.KZKT(KZ, K, KZKT));
+        assertArrayEquals(refKZKT.data, KZKT.data, 0.5e-4f);
     }
 
     @Test
     public void KZKT() {
-        int size = 100;
-        double nonZeroEltFraction = 0.12;
-        int numberOfCycles = 30;
-        int numberOfEdgesInCycle = 10;
-        int timesToRepeat = 200_000;
-
-        ZMatrixRMaj refZ = randomSymmetricalDenseMatrix(size, nonZeroEltFraction);
-        ZMatrixRMaj refK = randomDenseKMatrix(numberOfCycles, size, numberOfEdgesInCycle);
-        ZMatrixRMaj refKT = new ZMatrixRMaj(refK.numCols, refK.numRows);
-        ZMatrixRMaj refKZ = new ZMatrixRMaj(refK);
-        ZMatrixRMaj refKZKT = new ZMatrixRMaj(refK.numRows, refK.numRows);
         measureTimeMs("ref", timesToRepeat, () -> {
-            CommonOps_ZDRM.transpose(refK, refKT);
             CommonOps_ZDRM.mult(refK, refZ, refKZ);
-            CommonOps_ZDRM.mult(refKZ, refKT, refKZKT);
+            CommonOps_ZDRM.multTransB(refKZ, refK, refKZKT);
         });
-
-        ZMatrixCsr Z = zMatrixCsr(refZ);
-        IMatrixCsr K = iMatrixCsr(refK);
-        IMatrixCsr KT = new IMatrixCsr(K.numRows());
-        ZMatrixCsr KZ = new ZMatrixCsr(K.numCols());
-        ZMatrixRMaj KZKT = new ZMatrixRMaj(K.numRows(), K.numRows());
-        DynamicComplexArray tmpv = new DynamicComplexArray(K.numCols());
-        DynamicIntArray tmpc = new DynamicIntArray(K.numCols());
-        DynamicIntArray tmpi = new DynamicIntArray(K.numCols());
-        DcMatrixOps.transpose(K, KT);
-        measureTimeMs("ref1", timesToRepeat, () -> {
-            AcMatrixOps.KZKT(K, Z, KT, tmpv, tmpc, tmpi, KZKT);
+        measureTimeMs("act", timesToRepeat, () -> {
+            AcMatrixOps.KZ(K, Z, KZ);
+            KZKT = AcMatrixOps.KZKT(KZ, K, KZKT);
         });
-        System.out.printf("t1 = %.6f, t2 = %.6f\n", 1e-6 * AcMatrixOps.t1 / timesToRepeat, 1e-6 * AcMatrixOps.t2 / timesToRepeat);
-        AcMatrixOps.t1 = 0;
-        AcMatrixOps.t2 = 0;
-        measureTimeMs("ref2", timesToRepeat, () -> {
-            AcMatrixOps.KZ(K, Z, tmpi, tmpv, KZ);
-            AcMatrixOps.KZKT(KZ, KT, KZKT);
-        });
-        System.out.printf("t1 = %.6f, t2 = %.6f\n", 1e-6 * AcMatrixOps.t1 / timesToRepeat, 1e-6 * AcMatrixOps.t2 / timesToRepeat);
-
         assertArrayEquals(refKZKT.data, KZKT.data, 0.5e-6);
-    }
-
-    @Test
-    public void KxZ() {
-        int size = 100;
-        double nonZeroEltFraction = 0.12;
-        int numberOfCycles = 30;
-        int numberOfEdgesInCycle = 10;
-        int strideEstimation = (int) (size * nonZeroEltFraction);
-
-        ZMatrixRMaj denseZ = randomSymmetricalDenseMatrix(size, nonZeroEltFraction);
-        ZMatrixAc sparseZ = new ZMatrixAc(size, strideEstimation);
-        copyElts(sparseZ, denseZ);
-        ZMatrixCsr csrZ = zMatrixCsr(denseZ);
-        ZMatrixRMaj denseK = randomDenseKMatrix(numberOfCycles, size, numberOfEdgesInCycle);
-        KMatrix sparseK = new KMatrix(numberOfCycles, size);
-        copyElts(sparseK, denseK);
-        IMatrixCsr csrK = iMatrixCsr(denseK);
-
-        ZMatrixRMaj expected = new ZMatrixRMaj(numberOfCycles, size);
-        int timesToRepeat = 20_000;
-        double denseTime = measureTimeMillis(() -> CommonOps_ZDRM.mult(denseK, denseZ, expected), timesToRepeat);
-        ZMatrixRMaj actual = new ZMatrixRMaj(numberOfCycles, size);
-        double sparseTime = measureTimeMillis(() -> AcMatrixOps.mul(sparseK, sparseZ, actual), timesToRepeat);
-        double gustTime = measureTimeMillis(() -> AcMatrixOps.KZ(csrK, csrZ, actual), timesToRepeat);
-
-        DynamicIntArray tmpi = new DynamicIntArray(sparseZ.size());
-        DynamicComplexArray tmpz = new DynamicComplexArray(sparseZ.size());
-        ZMatrixCsr csrKZ = AcMatrixOps.KZ(csrK, csrZ, tmpi, tmpz, null);
-        double csrTime = measureTimeMillis(() -> {
-            AcMatrixOps.KZ(csrK, csrZ, tmpi, tmpz, csrKZ);
-        }, timesToRepeat);
-        assertArrayEquals(expected.data, actual.data, 0.5e-6);
-        assertArrayEquals(expected.data, rMaj(csrKZ), 0.5e-6);
-        logTime(denseTime, sparseTime, timesToRepeat);
-        System.out.printf("Густавсон с плотным результатом: %.6f\n", gustTime);
-        System.out.printf("Густавсон с результатом в CSR: %.6f\n", csrTime);
-        ZMatrixCsr KZT = new ZMatrixCsr(csrKZ.numCols());
-        measureTimeMillis(() -> AcMatrixOps.transpose(csrKZ, KZT), timesToRepeat, System.out::println);
-    }
-
-    @Test
-    void multGust() {
-        int numCols = 6;
-        int numRows = 4;
-
-        ZMatrixRMaj denseZ = randomSymmetricalDenseMatrix(numCols, 0.5);
-        ZMatrixAc sparseZ = new ZMatrixAc(numCols, 3);
-        copyElts(sparseZ, denseZ);
-        ZMatrixCsr csrZ = zMatrixCsr(denseZ);
-        ZMatrixRMaj denseK = randomDenseKMatrix(numRows, numCols, 3);
-        IMatrixCsr csrK = new IMatrixCsr(numCols);
-        copyElts(csrK, denseK);
-        ZMatrixRMaj actual = new ZMatrixRMaj(numRows, numCols);
-        ZMatrixRMaj expected = new ZMatrixRMaj(numRows, numCols);
-        CommonOps_ZDRM.mult(denseK, denseZ, expected);
-        AcMatrixOps.KZ(csrK, csrZ, actual);
-        assertArrayEquals(expected.data, actual.data, 0.5e-6);
-    }
-
-    @Test
-    public void KZxKTrans() {
-        int timesToRepeat = 200_000;
-        int numRows = 30;
-        int numCols = 100;
-        int numberOfEdgesInCycle = 10;
-        ZMatrixRMaj denseZ = randomSymmetricalDenseMatrix(numCols, 0.1);
-        ZMatrixRMaj denseK = randomDenseKMatrix(numRows, numCols, numberOfEdgesInCycle);
-        ZMatrixAc sparseZ = new ZMatrixAc(numCols, numCols / 10);
-        KMatrix sparseK = new KMatrix(numRows, numCols);
-        copyElts(sparseZ, denseZ);
-        copyElts(sparseK, denseK);
-        ZMatrixRMaj M = new ZMatrixRMaj(numRows, numCols);
-        AcMatrixOps.mul(sparseK, sparseZ, M);
-
-        ZMatrixRMaj expected = new ZMatrixRMaj(numRows, numRows);
-        ZMatrixRMaj actual = new ZMatrixRMaj(numRows, numRows);
-
-        double denseTime = measureTimeMillis(() -> CommonOps_ZDRM.multTransB(M, denseK, expected), timesToRepeat);
-        double sparseTime = measureTimeMillis(() -> AcMatrixOps.mulTransK(M, sparseK, actual), timesToRepeat);
-        assertArrayEquals(expected.data, actual.data, 0.5e-4f);
-        logTime(denseTime, sparseTime, timesToRepeat);
     }
 
     @Test
